93f1d0beac2ca5a8df1946c822839a09bf27ef71
EpsonRO <zxfarming010@gmail.com>
2026-04-03 02:24:47 +0800

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -64,7 +64,7 @@ $tools = @(
     # @{Model="L605"; Url=$null; File=$null; Exe=$null},
     # @{Model="L6160"; Url=$null; File=$null; Exe=$null},
     # @{Model="L6170"; Url=$null; File=$null; Exe=$null},
-    @{Model="L6190"; Url="https://github.com/EpsonRO/L6190/releases/download/L6190/L6190.zip"; File="L6190.zip"; Exe="AdjProg.exe"},
+    @{Model="L6190"; Url="https://github.com/EpsonRO/L6190/releases/download/L6190/L6190.zip"; File="L6190.zip"; Exe="AdjProg.exe"}
     # @{Model="L6270"; Url=$null; File=$null; Exe=$null},
     # @{Model="L6290"; Url=$null; File=$null; Exe=$null},
     # @{Model="L6490"; Url=$null; File=$null; Exe=$null},
@@ -79,7 +79,7 @@ $tools = @(
     # @{Model="L3110"; Url=$null; File=$null; Exe=$null},
     # @{Model="L3115"; Url=$null; File=$null; Exe=$null},
     # @{Model="L3116"; Url=$null; File=$null; Exe=$null},
-    @{Model="L3150"; Url= "https://github.com/EpsonRO/L6190/releases/download/L3150/L3150.zip"; File="L3150.zip"; Exe="AdjProg.exe"},
+    @{Model="L3150"; Url= "https://github.com/EpsonRO/L6190/releases/download/L3150/L3150.zip"; File="L3150.zip"; Exe="AdjProg.exe"}
     # @{Model="L3151"; Url=$null; File=$null; Exe=$null},
     # @{Model="L3156"; Url=$null; File=$null; Exe=$null},
     # @{Model="L1300"; Url=$null; File=$null; Exe=$null},
